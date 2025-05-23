749f1bda117fe229750f42cec84c564f41f5bcc8
Jonatas-Soares-Gama <jonatassoaresdagama@gmail.com>
2025-05-22 23:06:30 -0300

    Atualizando tabela

--- src/ingestion/explore_data.py
+++ src/ingestion/explore_data.py
@@ -22,15 +22,19 @@ df_sensors_by_countries = pd.DataFrame(sensor_list_by_countries)
 
 values_by_sensor = []
 def filter_values_by_sensor():
-    for sensor_id in df_sensors_by_countries['sensor_id']:
-        measurements = get_measurements(sensor_id)
-        for measurement in measurements.get('results', []):
+    for sensor_id in df_sensors_by_countries['sensor_id']: # for que passa por cada sensor_id da lista de sensores por país
+        measurements = get_measurements(sensor_id) # pega as medidas do sensor
+        for measurement in measurements.get('results', []): # for que passa por cada medida do sensor
             value = measurement.get('value')
-            parameter_id = measurement.get('parameter').get('id')
+            parameter_id = measurement.get('parameter').get('id') 
+            parameter_name = measurement.get('parameter').get('name')
+            date_time_utc = measurement.get('period', {}).get('datetimeFrom', {}).get('utc')
             values_by_sensor.append({
                 'sensor_id': sensor_id,
                 'parameter_id': parameter_id,
-                'value': value
+                'parameter_name': parameter_name,
+                'value': value,
+                'date_time_utc': date_time_utc
             })
 
     return values_by_sensor
