69afaf646a4fadaa0d57e3816bfdcdfe02d24cc2
Jonatas-Soares-Gama <jonatassoaresdagama@gmail.com>
2025-06-09 15:50:32 -0300

    update

--- dags/extract_dag.py
+++ dags/extract_dag.py
@@ -1,6 +1,34 @@
+import sys
+sys.path.append("/opt/airflow/src")
 from datetime import datetime
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
 
 from explore_data import filter_sensors_location, filter_values_by_sensor
+from openaq_ingestion import get_measurements, get_locations
+
+@dag(
+    dag_id="extract_dag",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+    tags=["extract"]
+)
+
+def extract_data():
+    @task()
+    def extract_raw_data():
+        return get_locations()
+    @task()
+    def extract_sensors_location():
+        return filter_sensors_location()
+    @task()
+    def transform_data():
+        df_sensors_by_countries = pd.DataFrame(filter_sensors_location())
+        df_sensors_by_countries.to_csv("/opt/airflow/data/sensors_by_countries.csv", index=False)
+    
+    extract_raw_data()
+    extract_sensors_location()
+    transform_data()
+    
+extract_data()
 
